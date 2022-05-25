bfe71ea21fbf2ff28884549fc9ecdecb49994b7c
rishabshalla <rishabshalla@gmail.com>
2022-05-25 19:45:01 +0530

    final update

--- Node2/server.js
+++ Node2/server.js
@@ -1,7 +1,7 @@
 const express = require("express"); // it will load the express package in our app
 const path = require("path");
 
-const port = 3000;
+const port = 8000;
 
 // Creating app Object
 const app = express();
@@ -9,16 +9,13 @@ const app = express();
 // Route Creation
 
 app.get("/", (req, res) => {
-  res.send("Elevation Academy");
+  res.send("Namaste Duniya");
 });
 
-app.get("/api/main", (req, res) => {
-  res.send("Hello World"); // text response
-  res.send("<h1>Hello World</h1>"); // html response
-  res.send({
-    name: "VISHAL",
-    age: 23,
-  }); // json response
+app.get("/home", (req, res) => {
+  // res.send("Hello World"); // text response
+  // res.send("<h1>Hello World</h1>"); // html response
+  // res.send({ name: "Rishab", age: 25 }); // json response
 
   const filePath = path.join(__dirname + "/index.html"); // the absolute path of my file
   res.sendFile(filePath); // file response
